92a40c7c44f8a8bfe4aec899191f44ef1628e5b9
Alberto Navalón <albertonl.dev@gmail.com>
2021-02-25 12:58:27 +0100

    add cses repetitions (1069)

---
+++ CSES/1069_repetitions.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  char prev = ' ', curr;
+  int count = 0, max = 0;
+
+  while (cin >> curr) {
+    if (curr != prev) {
+      if (count > max) max = count;
+      count = 1;
+      prev = curr;
+    } else count++;
+  }
+
+  if (max == 0 || count > max) max = count;
+  cout << max << "\n";
+
+  return 0;
+}
