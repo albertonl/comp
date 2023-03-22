1e7eaa535ea855461d497edfcb5d354c59595f49
Alberto Navalón <albertonl.dev@gmail.com>
2023-03-22 12:49:08 +0100

    (cses graph 1668) this actually works

--- CSES/7. Graphs/1668_teams.cpp
+++ CSES/7. Graphs/1668_teams.cpp
@@ -2,8 +2,27 @@
 
 #include <bits/stdc++.h>
 #pragma GCC optimize("O3")
-#define pb(x) push_back(x)
 using namespace std;
+
+int dfs(int idx, int cnum, vector<vector<int>>& adj, vector<bool>& visited, vector<int>& color) {
+    if (visited[idx]) return 0;
+    visited[idx] = true;
+    color[idx] = cnum;
+
+    for (int i = 0; i < adj[idx].size(); i++) {
+        if (color[adj[idx][i]] != 0 && color[adj[idx][i]] == cnum) {
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < adj[idx].size(); i++) {
+        if (dfs(adj[idx][i], ((cnum == 1) ? 2 : 1), adj, visited, color)) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
 
 int main () {
     ios_base::sync_with_stdio(false);
@@ -12,48 +31,30 @@ int main () {
     int n, m;
     cin >> n >> m;
 
-    vector<int> group(n+1, -1);
-    vector<bool> visited(n+1, false);
     vector<vector<int>> adj(n+1);
+    vector<bool> visited(n+1, false);
+    vector<int> color(n+1, 0);
 
     for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        adj[a].pb(b);
-        adj[b].pb(a);
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
 
     for (int i = 1; i <= n; i++) {
         if (visited[i]) continue;
-        queue<int> q;
-        int it = 1;
-        q.push(i);
-
-        while (!q.empty()) {
-            int x = q.front();
-            q.pop();
-
-            if (group[x] != -1) {
-                cout << "IMPOSSIBLE\n";
-                return 0;
-            }
-
-            if (visited[x]) continue;
-            visited[x] = true;
-            group[x] = it%2;
-            it++;
-            
-            for (int j = 0; j < adj[x].size(); j++) {
-                q.push(adj[x][j]);
-            }
+        if (dfs(i, 1, adj, visited, color)) {
+            cout << "IMPOSSIBLE\n";
+            return 0;
         }
     }
 
     for (int i = 1; i <= n; i++) {
-        if (i > 1) cout << " ";
-        cout << group[i];
+        if (i > 1) cout << ' ';
+        cout << color[i];
     }
 
-    cout << "\n";
+    cout << '\n';
     return 0;
 }
