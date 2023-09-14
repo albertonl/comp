5c2faf595cb2404b789089f8a543fcbaed8ee4a4
Alberto Navalón Lillo <43930380+albertonl@users.noreply.github.com>
2023-09-14 11:57:47 +0200

    warm-up

---
+++ CF/1791_teleporters.cpp
@@ -0,0 +1,41 @@
+// https://codeforces.com/problemset/problem/1791/G1
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+using namespace std;
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (; t > 0; --t) {
+        int n, c;
+        cin >> n >> c;
+
+        vector<int> a(n);
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i-1];
+            a[i-1] += i;
+        }
+
+        sort(a.rbegin(), a.rend());
+        // for (auto x : a) {
+        //     cout << x << " ";
+        // }
+        // cout << endl;
+        
+        int res = 0;
+        while (c >= a[a.size()-1] && a.size() > 0) {
+            c -= a[a.size()-1];
+            a.pop_back();
+            res++;
+        }
+
+        cout << res << '\n';
+    }
+
+    return 0;
+}
