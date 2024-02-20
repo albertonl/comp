46761e84e5ec6fc98cb35fb4f40d33366b192a54
Alberto Navalón <albertonl.dev@gmail.com>
2024-02-20 10:28:28 +0100

    CF 1932E

---
+++ CF/1932e_countdown.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+using namespace std;
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (; t > 0; --t) {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        vector<int> dp(n+1, 0);
+
+        for (int i = 1; i <= n; i++) dp[i] = dp[i-1] + (s[i-1] - '0');
+        for (int i = n; i > 0; i--) {
+            dp[i-1] += dp[i] / 10;
+            dp[i] %= 10;
+        }
+
+        bool foundFirstDigit = false;
+        for (int i = 0; i <= n; i++) {
+            if (!foundFirstDigit && dp[i] > 0) foundFirstDigit = true;
+            if (foundFirstDigit) cout << dp[i];
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
